7a95f74f05c89b399b86435fffbbbddfa4a27e85
pvosper <pvosper@mac.com>
2018-02-14 05:11:41 -0800

    Code Review changes: if __name__ == '__main__'; 'playlist_dictionary' instead of 'd'; simplify Classical exception

--- playlist_viewer.py
+++ playlist_viewer.py
@@ -1,11 +1,19 @@
 #!/usr/bin/env python
 
-"""Notes:
-- Ran into utf-8 error as file is utf-16
-- Song meta-data not consistent with album
-- Classical meta-data uses different attribution
+"""Given an iTunes playlist file, print a list of albums:
+<album> | <artist/composer> [Performed by <artist>]
 
-'album': [['artist'], ['performer'], ['year']]
+Example:
+
+python playlist_viewer.py playlist.txt
+
+Migration | Bonobo [Performed by Bonobo, 2017]
+
+
+Source:
+
+Name	Artist	Composer	Album	Grouping	Work	Movement Number	Movement Count	Movement Name	Genre	Size	Time	Disc Number	Disc Count	Track Number	Track Count	Year	Date Modified	Date Added	Bit Rate	Sample Rate	Volume Adjustment	Kind	Equalizer	Comments	Plays	Last Played	Skips	Last Skipped	My Rating	Location
+Migration	Bonobo		Migration						Electronic	11333649	327	1	1	1	12	2017	2/18/17, 5:31 PM	2/18/17, 11:19 AM	256	44100		Purchased AAC audio file			23	12/13/17, 2:59 PM	1	3/24/17, 8:43 AM		Macintosh HD:Users:paulvosper:Music:iTunes:iTunes Media:Music:Bonobo:Migration:01 Migration.m4a
 """
 
 
@@ -13,37 +21,40 @@ import csv
 import sys
 
 
-# max(arg1, ar2, *args[, key])
-# Return the largest item in an iterable
 def most_common(lst):
     return max(set(lst), key=lst.count)
 
 
-# with open(sys.argv[1], 'rt') as f:
-with open(sys.argv[1], encoding='utf-16') as f:
-    # reader = csv.reader(f)
-    reader = csv.DictReader(f, dialect='excel-tab')
+def print_playlist(playlist):
 
-    d = {}
+    with open(playlist, encoding='utf-16') as f:
+        reader = csv.DictReader(f, dialect='excel-tab')
 
-    for entry in reader:
-        album = entry['Album']
-        artist = entry['Artist']
-        performer = entry['Artist']
-        year = entry['Year']
-        # Classical differs in Artist/Performer vs Composer
-        if 'classical' in entry['Genre'].lower():
-            artist = entry['Composer']
-        if album in d:
-            d[album][0].append(artist)
-            d[album][1].append(performer)
-            d[album][2].append(year)
-        else:
-            d[album] = [[artist], [performer], [year]]
+        playlist_dictionary = {}
 
-    for entry in d:
-        album = entry
-        artist = most_common(d[entry][0])
-        performer = most_common(d[entry][1])
-        year = most_common(d[entry][2])
-        print('{} | {} [Performed by {}, {}]'.format(album, artist, performer, year))
+        # Create new playlist_dictionary from playlist file
+        for entry in reader:
+            album = entry['Album']
+            # Classical differs in Artist/Performer vs Composer
+            artist = entry['Composer'] if ('classical' in entry['Genre'].lower()) else entry['Artist']
+            performer = entry['Artist']
+            year = entry['Year']
+            if album in playlist_dictionary:
+                playlist_dictionary[album][0].append(artist)
+                playlist_dictionary[album][1].append(performer)
+                playlist_dictionary[album][2].append(year)
+            else:
+                playlist_dictionary[album] = [[artist], [performer], [year]]
+
+        # Print playlist dictionary
+        for entry in playlist_dictionary:
+            album = entry
+            artist = most_common(playlist_dictionary[entry][0])
+            performer = most_common(playlist_dictionary[entry][1])
+            year = most_common(playlist_dictionary[entry][2])
+            print('{} | {} [Performed by {}, {}]'.format(album, artist, performer, year))
+
+
+if __name__ == '__main__':
+
+    print_playlist(sys.argv[1])
